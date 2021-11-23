341f95ff1bd32dd3b7ce505f02c56198913ad162
QR1126 <1193523825@qq.com>
2021-11-23 14:34:18 +0800

    commit

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -3,11 +3,11 @@ package synthesizer;
 public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     protected int fillCount;
     protected int capacity;
-    @Override
+
     public int fillCount() {
         return this.fillCount;
     }
-    @Override
+
     public int capacity() {
         return this.capacity;
     }
