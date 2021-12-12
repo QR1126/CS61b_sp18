ab06d3daed17d5492c3336d8575d1162b06295c8
QR1126 <1193523825@qq.com>
2021-12-12 20:41:49 +0800

    lab11

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +12,40 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze m;
+    private boolean isFound = false;
+    private int[] nxtTo;
 
     public MazeCycles(Maze m) {
         super(m);
+        this.m = m;
+        nxtTo = new int[this.m.N() * this.m.N()];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(0);
     }
 
-    // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+        announce();
+        for (int w : maze.adj(v)) {
+            if (marked[w] && nxtTo[v] != w) {
+                announce();
+                isFound = true;
+            }
+        }
+        if (isFound) {
+            return;
+        }
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                nxtTo[w] = v;
+                dfs(w);
+            }
+        }
+    }
+
 }
 
