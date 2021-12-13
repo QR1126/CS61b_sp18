24b74f20f82058f6a4e9488bb22cb084ad528ba9
QR1126 <1193523825@qq.com>
2021-12-13 09:43:11 +0800

    lab11 done

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Comparator;
+
 /**
  *  @author Josh Hug
  */
@@ -8,6 +12,9 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+    private int N;
+    private MinPQ<Integer> pq;
+    private boolean isFound = false;
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -16,11 +23,27 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        N = m.N();
+        pq = new MinPQ<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return h(o1) - h(o2);
+            }
+        });
+        pq.insert(s);
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return Math.abs(toX(v) - toX(t)) + Math.abs(toY(v) - toY(t));
+    }
+
+    private int toX(int v) {
+        return v % N + 1;
+    }
+
+    private int toY(int v) {
+        return v / N + 1;
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -31,7 +54,24 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        while (!pq.isEmpty()) {
+            Integer v = pq.delMin();
+            marked[v] = true;
+            announce();
+            if (v == t) {
+                isFound = true;
+                announce();
+                break;
+            }
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    announce();
+                    pq.insert(w);
+                }
+            }
+        }
     }
 
     @Override
